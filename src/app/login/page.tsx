510d66bba1bd71848d167d9afc06888d2811822a
inguk <inguk@blast-team.com>
2023-12-06 15:30:10 +0900

    fix.로고 링크수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -33,7 +33,7 @@ const Index = () => {
       <div className="flex justify-center w-full h-full my-auto mt-12 xl:gap-14 lg:justify-normal md:gap-5 ">
         <div className="flex flex-col items-center justify-center w-3/5 gap-12 p-12 mx-auto bg-white rounded-lg shadow lg:w-1/3 lg:p-12">
           <Image
-            src="https://private-user-images.githubusercontent.com/122377401/287469285-4608a344-39ad-4d8d-ab53-201037272767.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDE1NDM5NDcsIm5iZiI6MTcwMTU0MzY0NywicGF0aCI6Ii8xMjIzNzc0MDEvMjg3NDY5Mjg1LTQ2MDhhMzQ0LTM5YWQtNGQ4ZC1hYjUzLTIwMTAzNzI3Mjc2Ny5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjAyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwMlQxOTAwNDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lMzk0OThlZDgwMTQ5MWIzZjg2NjM2NDcyYTk3ODhlNTM0NTE1NGVjYzQyZmI4NmJiOWQyYzZlMTE2NjdkZjY1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.2dH6rZ0rfQDvcyb8dmxA3eNxQiFMt2VrACLIqnYjypA"
+            src="/logo.png"
             alt="logo"
             width={250}
             height={200}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
         <div className="flex items-center gap-80">
           <a href="/">
             <Image
-              src="https://private-user-images.githubusercontent.com/122377401/287469285-4608a344-39ad-4d8d-ab53-201037272767.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDE1NDM5NDcsIm5iZiI6MTcwMTU0MzY0NywicGF0aCI6Ii8xMjIzNzc0MDEvMjg3NDY5Mjg1LTQ2MDhhMzQ0LTM5YWQtNGQ4ZC1hYjUzLTIwMTAzNzI3Mjc2Ny5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjAyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwMlQxOTAwNDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lMzk0OThlZDgwMTQ5MWIzZjg2NjM2NDcyYTk3ODhlNTM0NTE1NGVjYzQyZmI4NmJiOWQyYzZlMTE2NjdkZjY1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.2dH6rZ0rfQDvcyb8dmxA3eNxQiFMt2VrACLIqnYjypA"
+              src="/logo.png"
               alt="logo"
               width={200}
               height={150}
