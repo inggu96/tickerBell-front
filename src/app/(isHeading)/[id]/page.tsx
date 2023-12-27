1b51894939594ed1b41501af17702a8568929405
inguk <inguk@blast-team.com>
2023-12-27 21:23:42 +0900

    fix.빌드테스트

--- src/app/(isHeading)/[id]/page.tsx
+++ src/app/(isHeading)/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { getEventCategoryApi, getEventIdApi } from "@/api/events";
 import Card from "@/components/item/Card";
+import apiInstance from "@/util/useInterceptor";
 import { useQuery } from "@tanstack/react-query";
 import {
   useParams,
@@ -11,17 +12,17 @@ import {
 } from "next/navigation";
 import React from "react";
 
-// export function generateStaticParams() {
-//   const categorys = ["musical", "concert", "play", "classic", "sports"];
-//   return categorys.map((category) => ({
-//     slug: category,
-//   }));
-// }
-
-// export function generateStaticParams() {
-//   return [{ slug: ["a", "1"] }, { slug: ["b", "2"] }, { slug: ["c", "3"] }];
-// }
-
+export async function generateStaticParams() {
+  // usePathname 훅을 사용할 수 없으므로, 라우트 경로를 직접 입력합니다.
+  const id = "app/(isHeading)/[id]/page.tsx";
+  // fetch 함수를 await 키워드로 기다립니다.
+  const res = await apiInstance.get(`/api/event/${id}`);
+  const data = res.data;
+  const slugs = data.map((item: any) => item.slug);
+  return slugs.map((slug: any) => ({
+    slug,
+  }));
+}
 const Index = () => {
   const path = usePathname();
   const id = path.slice(1);
@@ -31,7 +32,7 @@ const Index = () => {
     queryFn: () => getEventCategoryApi(1, id.toString().toUpperCase(), ""),
   });
 
-  // console.log("카테고리 : ", data?.data.eventListResponses);
+  console.log("카테고리 : ", data?.data.eventListResponses);
 
   return (
     <div className="grid grid-cols-6 gap-36 place-items-center mt-60">
