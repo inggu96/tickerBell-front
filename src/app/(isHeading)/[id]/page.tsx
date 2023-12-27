dbf565ec2a3ff569866f401f4bd2851e3024d17f
inguk <inguk@blast-team.com>
2023-12-27 19:01:18 +0900

    빌드테스트

--- src/app/(isHeading)/[id]/page.tsx
+++ src/app/(isHeading)/[id]/page.tsx
@@ -1,39 +1,45 @@
-// "use client";
+"use client";
 
 import { getEventCategoryApi, getEventIdApi } from "@/api/events";
 import Card from "@/components/item/Card";
 import { useQuery } from "@tanstack/react-query";
-import { useParams, useServerInsertedHTML } from "next/navigation";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useServerInsertedHTML,
+} from "next/navigation";
 import React from "react";
 
-export function generateStaticParams() {
-  const categorys = ["musical", "concert", "play", "classic", "sports"];
-  return categorys.map((category) => ({
-    slug: category,
-  }));
-}
+// export function generateStaticParams() {
+//   const categorys = ["musical", "concert", "play", "classic", "sports"];
+//   return categorys.map((category) => ({
+//     slug: category,
+//   }));
+// }
 
 // export function generateStaticParams() {
 //   return [{ slug: ["a", "1"] }, { slug: ["b", "2"] }, { slug: ["c", "3"] }];
 // }
 
-const Index = ({ params }: { params: { slug: string } }) => {
-  const { slug } = params;
+const Index = () => {
+  const path = usePathname();
+  const id = path.slice(1);
 
-  // const { data, isSuccess, isError, error } = useQuery({
-  //   queryKey: ["event-category", slug],
-  //   queryFn: () => getEventCategoryApi(1, slug.toString().toUpperCase(), ""),
-  // });
+  const { data, isSuccess, isError, error } = useQuery({
+    queryKey: ["event-category", id],
+    queryFn: () => getEventCategoryApi(1, id.toString().toUpperCase(), ""),
+  });
 
   // console.log("카테고리 : ", data?.data.eventListResponses);
 
   return (
     <div className="grid grid-cols-6 gap-36 place-items-center mt-60">
-      {/* {isSuccess &&
+      {isSuccess &&
         data?.data.eventListResponses.map((item: any, index: any) => (
           <Card key={index} data={item} />
-        ))} */}
-      디테일{slug}
+        ))}
+      디테일{id}
     </div>
   );
 };
